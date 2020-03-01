0625079c2fe32799a9a22848eaf1333afc00984d
nikita <pantyushkinnikita555@gmail.com>
2020-03-01 18:17:03 +0500

    Добавлена заготовка

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import pygame
+import pygame, random
 
-print('...')
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+fps = 50
+clock = pygame.time.Clock()
+running = True
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(pygame.Color(0, 0, 0))
+    pygame.display.flip()
+    clock.tick(fps)
